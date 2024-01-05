fbb4fddfbdbd090d92e0b0bd7286c42668636cf1
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-05 21:29:46 +0530

    Implement displaying list

--- src/Components/DisplayFolder/DisplayList.jsx
+++ src/Components/DisplayFolder/DisplayList.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './DisplayList.css';
 
-const DisplayList = () => {
+const DisplayList = (props) => {
     return (
-        <div>DisplayList</div>
+        <div>
+            {props.data.map(item => (
+                <ul key={item.id}>
+                    <li>
+                        {item.name} ({item.age} years old)
+                        <button style={{ marginLeft: '20px' }} onClick={() => props.onDelete(item.id)}>Delete</button>
+                    </li>
+                </ul>
+            ))}
+        </div>
     )
 }
 
-export default DisplayList
+export default DisplayList;
