a9bf6fba9a6b4cd4ff88c7345d4f044af10cf637
AvadhutK01 <kelaskaravadhut052@gmail.com>
2024-01-05 21:18:41 +0530

    Created input form for getting inputs from user

--- src/Components/InputFolder/InputForm.jsx
+++ src/Components/InputFolder/InputForm.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './InputForm.css';
 
-const InputForm = () => {
+const InputForm = (props) => {
+    let [name, setName] = useState('');
+    let [age, setAge] = useState('');
+
+    const nameChangeHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const ageChangeHandler = (event) => {
+        setAge(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        return props.onSubmit(name, age);
+    }
+
     return (
-        <div>InputForm</div>
+        <form onSubmit={submitHandler}>
+            <div>
+                <input type='text' placeholder='Enter a name' onChange={nameChangeHandler} />
+                <input type='number' placeholder='Enter age' onChange={ageChangeHandler} />
+                <button type='submit'>Submit</button>
+            </div>
+        </form>
     )
 }
 
